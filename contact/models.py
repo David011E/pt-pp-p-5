f480741db8959714c746854a74bf85c199ac829e
David011e <davidoliver011e@gmail.com>
2024-04-25 23:17:37 +0100

    Setup models for contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    description = models.TextField()
