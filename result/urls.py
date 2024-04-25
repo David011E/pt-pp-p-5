1c8434b4e0a69f249b4ebfb7a2a07eb66e2db790
David011e <davidoliver011e@gmail.com>
2024-04-25 16:03:06 +0100

    Add review details and add review functions and template

--- result/urls.py
+++ result/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.results, name='result'),
+    path('add/', views.add_review, name='add_review'),
+    path('<int:reviews_id>/', views.review_details, name='review_details'),
 ]
--- result/views.py
+++ result/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Review 
+from .forms import ReviewForm
 
+import sweetify
+
+# Create your views here.
 def results(request):
     """
     A view to return index page with reviews
@@ -10,3 +14,40 @@ def results(request):
         'reviews': reviews,
     }
     return render(request, 'result/result.html', context)
+
+
+def add_review(request):
+    """ Add a review to the store """
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            sweetify.success(request, 'Successfully added review!')
+            return redirect(reverse('result'))
+        else:
+            sweetify.error(request, 'Failed to add review. Please ensure the form is valid.')
+    else:
+        form = ReviewForm()
+
+    form = ReviewForm()
+    template = 'result/add_review.html'
+    context = {
+        'form': form
+    }
+
+    return render(request, template, context)
+
+
+def review_details(request, reviews_id):
+    """
+    A view to show individual review details
+    """
+
+    review = get_object_or_404(Review, pk=reviews_id)  # Retrieve a single review object
+
+    context = {
+        'review': review,  # Pass the single review object to the template with the correct key
+    }
+
+    return render(request, 'result/review_details.html', context)
