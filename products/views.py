fc0fa70ebe2764f83b3d91a02a0204364ad72c8d
David011e <davidoliver011e@gmail.com>
2024-04-25 09:21:10 +0100

    Setup templates for products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 # Create your views here.
+def all_services(request):
+
+    products = Product.objects.all()
+
+    templates = 'products/products.html'
+
+    context = {
+        'products': products,
+        'templates': templates,
+    }
+
+    return render(request, templates, context)
