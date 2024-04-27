4c4ca94dad847350a323d6dadc426b37e6b77c06
David011e <davidoliver011e@gmail.com>
2024-04-27 04:48:45 +0100

    Set STATIC_ROOT and configure static files settings

--- pt/settings.py
+++ pt/settings.py
@@ -182,6 +182,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
